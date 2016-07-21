a7fec849b85621a788e12441da67061984b2f044
drcyber96 <muhdnajmiey89@gmail.com>
2016-07-21 15:56:35 +0800

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -6,6 +6,10 @@ OS=`uname -p`;
 # go to root
 cd
 
+# disable se linux
+echo 0 > /selinux/enforce
+sed -i 's/SELINUX=enforcing/SELINUX=disable/g'  /etc/sysconfig/selinux
+
 # set locale
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service sshd restart
@@ -18,23 +22,6 @@ sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.d/rc.loca
 # install wget and curl
 yum -y install wget curl
 
-# setting repo
-wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-rpm -Uvh epel-release-6-8.noarch.rpm
-rpm -Uvh remi-release-6.rpm
-
-if [ "$OS" == "x86_64" ]; then
-  wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-  rpm -Uvh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-else
-  wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.i686.rpm
-  rpm -Uvh rpmforge-release-0.5.3-1.el6.rf.i686.rpm
-fi
-
-sed -i 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/rpmforge.repo
-sed -i -e "/^\[remi\]/,/^\[.*\]/ s|^\(enabled[ \t]*=[ \t]*0\\)|enabled=1|" /etc/yum.repos.d/remi.repo
-rm -f *.rpm
 
 # remove unused
 yum -y remove sendmail;
@@ -63,10 +50,9 @@ service exim stop
 chkconfig exim off
 
 # setting vnstat
-vnstat -u -i venet0
+vnstat -u -i eth0
 echo "MAILTO=root" > /etc/cron.d/vnstat
 echo "*/5 * * * * root /usr/sbin/vnstat.cron" >> /etc/cron.d/vnstat
-sed -i 's/eth0/venet0/g' /etc/sysconfig/vnstat
 service vnstat restart
 chkconfig vnstat on
 
@@ -80,32 +66,33 @@ echo "screenfetch" >> .bash_profile
 
 # install webserver
 cd
-wget -O /etc/nginx/nginx.conf "https://raw.github.com/drcyber96/autoscript/master/conf/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/nginx.conf"
 sed -i 's/www-data/nginx/g' /etc/nginx/nginx.conf
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by KangArie | JualSSH.com | @arieonline | 7946F434</pre>" > /home/vps/public_html/index.html
+echo "<pre>DRCYBER</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 rm /etc/nginx/conf.d/*
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/drcyber96/autoscript/master/conf/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/vps.conf"
 sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
 chmod -R +rx /home/vps
 service php-fpm restart
 service nginx restart
 
 # install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/drcyber96/autoscript/master/conf/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/drcyber96/autoscript/master/conf/1194-centos.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/1194-centos.conf"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /etc/openvpn/1194.conf "https://raw.github.com/drcyber96/autoscript/master/conf/1194-centos64.conf"
+  wget -O /etc/openvpn/1194.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/1194-centos64.conf"
 fi
-wget -O /etc/iptables.up.rules "https://raw.github.com/drcyber96/autoscript/master/conf/iptables.up.rules"
+wget -O /etc/iptables.up.rules "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 sed -i $MYIP2 /etc/iptables.up.rules;
+sed -i 's/venet0/eth0/g' /etc/iptables.up.rules
 iptables-restore < /etc/iptables.up.rules
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
@@ -115,21 +102,21 @@ cd
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/drcyber96/autoscript/master/conf/1194-client.conf"
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/1194-client.conf"
 sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false drcyber96
-echo "drcyber96:$PASS" | chpasswd
-echo "drcyber96" > pass.txt
+useradd -M -s /bin/false anas
+echo "anas:$PASS" | chpasswd
+echo "anas" > pass.txt
 echo "$PASS" >> pass.txt
 tar cf client.tar 1194-client.ovpn pass.txt
 cp client.tar /home/vps/public_html/
 cd
 
 # install badvpn
-wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/drcyber96/autoscript/master/conf/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/drcyber96/autoscript/master/conf/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
@@ -138,15 +125,15 @@ screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
 # install mrtg
 cd /etc/snmp/
-wget -O /etc/snmp/snmpd.conf "https://raw.github.com/drcyber96/autoscript/master/conf/snmpd.conf"
-wget -O /root/mrtg-mem.sh "https://raw.github.com/drcyber96/autoscript/master/conf/mrtg-mem.sh"
+wget -O /etc/snmp/snmpd.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/snmpd.conf"
+wget -O /root/mrtg-mem.sh "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/mrtg-mem.sh"
 chmod +x /root/mrtg-mem.sh
 service snmpd restart
 chkconfig snmpd on
 snmpwalk -v 1 -c public localhost | tail
 mkdir -p /home/vps/public_html/mrtg
 cfgmaker --zero-speed 100000000 --global 'WorkDir: /home/vps/public_html/mrtg' --output /etc/mrtg/mrtg.cfg public@localhost
-curl "https://raw.github.com/drcyber96/autoscript/master/conf/mrtg.conf" >> /etc/mrtg/mrtg.cfg
+curl "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/mrtg.conf" >> /etc/mrtg/mrtg.cfg
 sed -i 's/WorkDir: \/var\/www\/mrtg/# WorkDir: \/var\/www\/mrtg/g' /etc/mrtg/mrtg.cfg
 sed -i 's/# Options\[_\]: growright, bits/Options\[_\]: growright/g' /etc/mrtg/mrtg.cfg
 indexmaker --output=/home/vps/public_html/mrtg/index.html /etc/mrtg/mrtg.cfg
@@ -176,8 +163,7 @@ tar xf vnstat_php_frontend-1.5.1.tar.gz
 rm vnstat_php_frontend-1.5.1.tar.gz
 mv vnstat_php_frontend-1.5.1 vnstat
 cd vnstat
-sed -i 's/eth0/venet0/g' config.php
-sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
 sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
 sed -i 's/Internal/Internet/g' config.php
 sed -i '/SixXS IPv6/d' config.php
@@ -190,7 +176,7 @@ chkconfig fail2ban on
 
 # install squid
 yum -y install squid
-wget -O /etc/squid/squid.conf "https://raw.github.com/drcyber96/autoscript/master/conf/squid-centos.conf"
+wget -O /etc/squid/squid.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/squid-centos.conf"
 sed -i $MYIP2 /etc/squid/squid.conf;
 service squid restart
 chkconfig squid on
@@ -205,37 +191,31 @@ chkconfig webmin on
 
 # pasang bmon
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/bmon "https://raw.github.com/drcyber96/autoscript/master/conf/bmon64"
+  wget -O /usr/bin/bmon "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/bmon64"
 else
-  wget -O /usr/bin/bmon "https://raw.github.com/drcyber96/autoscript/master/conf/bmon"
+  wget -O /usr/bin/bmon "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/bmon"
 fi
 chmod +x /usr/bin/bmon
 
 # downlaod script
 cd
 wget -O speedtest_cli.py "https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py"
-wget -O bench-network.sh "https://raw.github.com/drcyber96/autoscript/master/conf/bench-network.sh"
+wget -O bench-network.sh "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/bench-network.sh"
 wget -O ps_mem.py "https://raw.github.com/pixelb/ps_mem/master/ps_mem.py"
-wget -O limit.sh "https://raw.github.com/drcyber96/autoscript/master/conf/limit.sh"
-curl http://script.jualssh.com/user-login.sh > user-login.sh
-curl http://script.jualssh.com/user-expire.sh > user-expire.sh
-curl http://script.jualssh.com/user-limit.sh > user-limit.sh
+wget -O limit.sh "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/limit.sh"
 echo "0 0 * * * root /root/user-expire.sh" > /etc/cron.d/user-expire
 sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.local
 sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.d/rc.local
 chmod +x bench-network.sh
 chmod +x speedtest_cli.py
 chmod +x ps_mem.py
-chmod +x user-login.sh
-chmod +x user-expire.sh
-chmod +x user-limit.sh
-chmod +x limit.sh
+
 
 # cron
 service crond start
 chkconfig crond on
 
-# Change to Time GMT+8
+# set time GMT +8
 ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 
 # finalisasi
